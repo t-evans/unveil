d2aad1d9ce56ed4cd8b4b20530e59c704930d3e2
troy <troy@nutrislice.com>
2013-06-18 16:16:35 -0600

    Added additional options
    Added the following options:
    - $preLoadWrapper
    - $postLoadWrapper
    - $preLoadContent (some content to show in place of the image while
    waiting for it to load)
    - onLoad

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -9,22 +9,66 @@
  */
 
 ;(function($) {
+  var defaults = {
+    threshold: 0,
+    $preLoadContent: null, // A jQuery object that will be shown in place of the img, while it is waiting for the img to load
+    $preLoadWrapper: null, // A jQuery object that the initial image should be placed in while waiting
+    $postLoadWrapper: null, // A jQuery object that the final image should be placed in
+    hideOnTransition: true, // By default, it will hide & fade in the new image. Override this and onLoad if you would like to change that.
+    onLoad: function($img) {
+        $img.fadeIn();
+    }
+  };
+  $.fn.unveil = function(opts) {
 
-  $.fn.unveil = function(threshold) {
-
-    var $w = $(window),
-        th = threshold || 0,
+    var opts = $.extend(defaults, opts),
+        $w = $(window),
+        th = opts.threshold,
         retina = window.devicePixelRatio > 1,
         attrib = retina? "data-src-retina" : "data-src",
         images = this,
+        onLoad = opts.onLoad,
         loaded,
         inview,
         source;
 
     this.one("unveil", function() {
+      var $img = $(this);
+
+      if (opts.$preLoadContent) {
+        $img.hide();
+        $img.before(opts.$preLoadContent.addClass('_unveil-preloadcontent'));
+      }
+      else if (opts.$preLoadWrapper)
+        $img.wrap(opts.$preLoadWrapper.addClass('_unveil-preloadwrapper'));
+
       source = this.getAttribute(attrib);
       source = source || this.getAttribute("data-src");
-      if (source) this.setAttribute("src", source);
+      if (source) {
+        $img.load(function() {
+          var $tempImg = $img.clone(),
+              $preLoadContent = $img.prev('._unveil-preloadcontent'),
+              $preLoadWrapper = $img.parent('._unveil-preloadwrapper'),
+              $newImg;
+          if (opts.$postLoadWrapper) {
+            var $postLoadWrapper = opts.$postLoadWrapper.clone();
+            $postLoadWrapper.append($tempImg);
+            $newImg = $postLoadWrapper;
+          }
+          else
+            $newImg = $tempImg;
+          if (opts.hideOnTransition)
+            $newImg.hide();
+          if ($preLoadWrapper.length)
+            $preLoadWrapper.replaceWith($newImg);
+          else if ($preLoadContent.length)
+            $preLoadContent.replaceWith($newImg);
+          else
+            $img.replaceWith($newImg);
+          if (onLoad)
+            onLoad($newImg);
+        }).attr('src', source);
+      }
     });
 
     function unveil() {
